9d20f2330645ff1e79f43711e5dfa7ab6e3c3585
Tehila <99tehila@gmail.com>
2019-09-18 16:24:01 +0300

    ufter adding gitignore

--- Dal1/DBConnect.cs
+++ Dal1/DBConnect.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,12 @@ namespace Dal1
 {
     public class DBConnect
     {
-
-        DialogflowDataEntities db = new DialogflowDataEntities();
         MySqlConnectionStringBuilder connectionString;
         MySqlConnection connection;
-        MySqlCommand command;
-
         public DBConnect()
         {
             Initialize();
         }
-
         public void Initialize()
         {
             connectionString = new MySqlConnectionStringBuilder
@@ -31,40 +27,37 @@ namespace Dal1
                 UserID = "root",
                 Password = "heartbot1234",
                 Database = "heartbotdata",
-                CertificateFile = @"C:\Users\תהילה\Downloads\client.pfx",
-                SslCa = @"C:\Users\תהילה\Downloads\server-ca.pem",
+                CertificateFile =Directory.GetCurrentDirectory()+ @"\client.pfx",
+                SslCa = Directory.GetCurrentDirectory() + @"\server-ca.pem",
                 SslMode = MySqlSslMode.None,
             };
             connection = new MySqlConnection(connectionString.ConnectionString);
         }
-
-         
-        public void insert()
+        public List<userWeb> getAllNames()
         {
-            string insertCommand = "INSERT INTO user(id,iduser,name,date,score,url,checked) VALUES(0,123,'Avi',9/9/2019,20,'http://www.mysqltutorial.org/mysql-group-by.aspx',true)";
-        }
-        public List<userWeb> getAllName()
-        {
-            // string insertCommand = "INSERT INTO user(id,iduser,name,date,score,url,checked) VALUES(@id,@iduser,@name,@date,@score,@url,@checked)";
             List<userWeb>  users= new List<userWeb>();
-            connection.Open();
-            MySqlCommand cmd = connection.CreateCommand();
-            cmd.CommandText = "SELECT * FROM USER";
-            MySqlDataReader dataReader = cmd.ExecuteReader();
-            while (dataReader.Read()==true)
+            try
             {
-                userWeb u = new userWeb();
-                u.id = int.Parse(dataReader["id"].ToString());
-                u.idUser = dataReader["userid"].ToString();
-                u.name = dataReader["name"].ToString();
-                u.url = dataReader["url"].ToString();
-                u.@checked =bool.Parse(dataReader["checked"].ToString());
-                u.date = dataReader["date"].ToString();
-                u.score = int.Parse(dataReader["score"].ToString());
-                users.Add(u);
+                connection.Open();
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM user";
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read() == true)
+                {
+                    userWeb u = new userWeb();
+                    u.id = int.Parse(dataReader["id"].ToString());
+                    u.idUser = dataReader["userid"].ToString();
+                    u.name = dataReader["name"].ToString();
+                    u.url = dataReader["url"].ToString();
+                    u.@checked = bool.Parse(dataReader["checked"].ToString());
+                    u.date = dataReader["date"].ToString();
+                    u.score = int.Parse(dataReader["score"].ToString());
+                    users.Add(u);
+                }
+                users.GroupBy(x => x.idUser).Select(x => x.First()).OrderBy(x => x.name).ToList();
+                return users;
             }
-            users.GroupBy(x => x.idUser).Select(x => x.First()).OrderBy(x=>x.name).ToList();
-            return users;
+            catch (Exception e) { throw e; }
         }
         public List<userWeb> getpdfByname(string iduser)
         {
@@ -141,17 +134,15 @@ namespace Dal1
             }
     
         }
-
-
-        public void Post(user user)
+        public void Post(userWeb user)
         {
             try
             {
-                using (DialogflowDataEntities db = new DialogflowDataEntities())
-                {
-                    db.user.Add(user);
-                    db.SaveChanges();
-                }
+                connection.Open();
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO user(id,iduser,name,date,score,url,checked) VALUES(" + user.id + "," + user.idUser + "," + user.name + "," + user.date + "," + user.score + "," + user.url + "," + user.@checked + "," + ")";
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                connection.Close();
             }
             catch (Exception)
             {
--- DialogFlowWebApi/Controllers/usersController.cs
+++ DialogFlowWebApi/Controllers/usersController.cs
@@ -19,14 +19,14 @@ namespace DialogFlowWebApi.Controllers
     {
         
         [HttpGet]
-        [Route("getAllName")]
-        public IEnumerable<userWeb> getAllName()
+        [Route("getAllNames")]
+        public IEnumerable<userWeb> getAllNames()
         {
 
             DBConnect c = new DBConnect();
             try
             {
-               return c.getAllName();
+               return c.getAllNames();
             }
             catch (Exception e) { throw e; }
         }
@@ -70,81 +70,26 @@ namespace DialogFlowWebApi.Controllers
                 throw;
             }
         }
-
-        //public IHttpActionResult Post([FromBody] JObject pd)
-        //{
-
-        //    users user = new users();
-        //    user.id = 0;
-        //    user.name = (string)pd.SelectToken("personalDetails").SelectToken("name").SelectToken("name1");
-        //    user.age = (int)pd.SelectToken("personalDetails").SelectToken("age").SelectToken("amount");
-        //    user.gender = (string)pd.SelectToken("personalDetails").SelectToken("gender");
-        //    user.isDiabetes = string.Equals((string)pd.SelectToken("heartRelatedDiseases").SelectToken("diabetes"), "diabetes");
-        //    user.isCholesterol = string.Equals((string)pd.SelectToken("heartRelatedDiseases").SelectToken("cholesterol"), "cholesterol");
-        //    user.heart_disease = (string)pd.SelectToken("heartRelatedDiseases").SelectToken("heart_disease");
-        //    user.isObesity = string.Equals((string)pd.SelectToken("obesityAndExercise").SelectToken("obesity"), "obesity");
-        //    user.isExercise = string.Equals((string)pd.SelectToken("obesityAndExercise").SelectToken("exercise"), "exercise");
-        //    user.exerciseOften = (string)pd.SelectToken("obesityAndExercise").SelectToken("exercise_often");
-        //    user.dateExercise = (string)pd.SelectToken("obesityAndExercise").SelectToken("date-period");
-        //    user.isDrug = string.Equals((string)pd.SelectToken("drugs").SelectToken("drugs"), "drugs");
-        //    user.nameDrug = (string)pd.SelectToken("nameDrug");
-        //    user.isSmoking = string.Equals((string)pd.SelectToken("smokingHabits").SelectToken("smoke"), "smoke"); ;
-        //    user.oftenSmoke = (string)pd.SelectToken("smokingHabits").SelectToken("SmokingOften");
-        //    user.brandSmoke = (string)pd.SelectToken("smokingHabits").SelectToken("SmokingType");
-        //    user.amountTypeSmoke = (string)pd.SelectToken("smokingHabits").SelectToken("SmokingAmountType");
-        //    user.amountSmoke = (string)pd.SelectToken("smokingHabits").SelectToken("SmokingAmount");
-        //    user.painLevel = (string)pd.SelectToken("generalFeeling").SelectToken("painLevel");
-        //    user.feeling = (string)pd.SelectToken("generalFeeling").SelectToken("feeling");
-        //    user.bloodPressure = (string)pd.SelectToken("generalFeeling").SelectToken("bloodPressure");
-        //    user.trauma = (string)pd.SelectToken("generalFeeling").SelectToken("trauma");
-        //    user.vomitAmount = (string)pd.SelectToken("generalFeeling").SelectToken("vomitAmount");
-        //    user.feverDgree = (string)pd.SelectToken("generalFeeling").SelectToken("feverDgree");
-        //    user.painLocation = (string)pd.SelectToken("generalFeeling").SelectToken("painLocation");
-        //    user.timeOfPain = (string)pd.SelectToken("generalFeeling").SelectToken("timeOfPain");
-        //    user.havaFever = (string)pd.SelectToken("generalFeeling").SelectToken("havaFever");
-        //    user.disease = (string)pd.SelectToken("generalFeeling").SelectToken("disease");
-        //    user.diastolic= (int)pd.SelectToken("generalFeeling").SelectToken("diastolic");
-        //    user.systolic = (int)pd.SelectToken("generalFeeling").SelectToken("systolic");
-        //    try
-        //    {
-        //        Class1 c = new Class1();
-
-        //        user = Class1.Score(user);
-        //        c.Post(user);
-        //        return Ok();
-        //    }
-        //    catch (Exception e) { throw e; }
-        //}
         [HttpPost]
         [Route("addUser/{pd}")]
         public IHttpActionResult Post([FromBody] JObject pd)
         {
-            user user = new user();
+            userWeb user = new userWeb();
+            string userid = (string)pd.SelectToken("userid");
+            user.id = 0;
+            user.@checked = false;
+            user.date = DateTime.Now.ToString();
+            user.url = "https://storage.cloud.google.com/myheartpdfbucket/" + userid + ".html";
+            user.idUser = userid;
+            user.name = (string)pd.SelectToken("name");
+            user.score = (int)pd.SelectToken("score");
             try
             {
                 DBConnect c = new DBConnect();
-
-                //user = Class1.Score(user);
-                //c.Post(user);
+                c.Post(user);
                 return Ok();
             }
             catch (Exception e) { throw e; }
         }
-    //    user user = new user();
-    //    string userid = (string)pd.SelectToken("userid");
-    //    user.id = 0;
-    //        user.@checked = false;
-    //        user.date =DateTime.Now.ToString();
-    //        user.url = "https://storage.cloud.google.com/myheartpdfbucket/"+ userid+".html";
-    //        user.idUser = userid;
-    //        user.name = (string) pd.SelectToken("name");
-    //    user.score= (int) pd.SelectToken("score");
-    //        try
-    //        {
-    //            DBConnect c = new DBConnect();
-    //    c.Post(user);
-    //            return Ok();
-    //}
-    //        catch (Exception e) { throw e; }
     }
 }
